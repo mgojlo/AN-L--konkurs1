464559bd1114343cf2153a3149705425d07c98d2
Maksymilian Gojło <347809@uwr.edu.pl>
2025-04-08 00:29:43 +0200

    Colors: license

--- src/colorscheme.zig
+++ src/colorscheme.zig
@@ -9,6 +9,37 @@ fn colorFromInt(color: u24) rl.Color {
     };
 }
 
+const base = blueforest;
+
+pub const palette = struct {
+    pub const colors = base;
+    pub const bg_colors = base[0..8];
+    pub const fg_colors = base[8..16];
+};
+
+// Color palettes from base 16
+// MIT License
+//
+// Copyright (c) 2024 Tinted Theming
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 const aztec = &[_]rl.Color{
     colorFromInt(0x101600),
     colorFromInt(0x1A1E01),
@@ -64,12 +95,4 @@ const simple = &[_]rl.Color{
     rl.Color.dark_blue,
     rl.Color.purple,
     rl.Color.dark_purple,
-};
-
-const base = blueforest;
-
-pub const palette = struct {
-    pub const colors = base;
-    pub const bg_colors = base[0..8];
-    pub const fg_colors = base[8..16];
 };
